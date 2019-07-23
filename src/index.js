bf36d3ffc68e84815f5239546f44f5a7f3def650
Dhaval <dhaval.makvana@techmatters.com>
2019-07-24 05:04:54 +0530

    todo list reducer - adding a todo case

--- src/index.js
+++ src/index.js
@@ -2,7 +2,50 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
+const todos = (state = [], action) => {
+    switch (action.type) {
+        case 'ADD_TODO':
+            return [
+                ...state,
+                {
+                    id: action.id,
+                    text: action.text,
+                    completed: false
+                }
+            ];
+        default:
+            return state;
+    }
+};
+
+const testAddTodo = () => {
+    const stateBefore = [];
+    const action = {
+        type: 'ADD_TODO',
+        id: 0,
+        text: 'Learn Redux'
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+};
+
+testAddTodo();
+console.log('All tests passed.');
 
 const TodoApp = () => (<div>Initial Refactor Commit</div>)
 
