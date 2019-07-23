236f09081922ffd2f2c1580be84a7fd76a0ab330
Dhaval <dhaval.makvana@techmatters.com>
2019-07-24 05:12:01 +0530

    todo list reducer - toggle todo case

--- src/index.js
+++ src/index.js
@@ -16,6 +16,16 @@ const todos = (state = [], action) => {
                     completed: false
                 }
             ];
+        case 'TOGGLE_TODO':
+            return state.map((todo) => {
+                if (todo.id !== action.id) {
+                    return todo;
+                }
+                return {
+                    ...todo,
+                    completed: !todo.completed
+                }
+            })
         default:
             return state;
     }
@@ -44,7 +54,46 @@ const testAddTodo = () => {
     ).toEqual(stateAfter);
 };
 
+const testToggleTodo = () => {
+    const stateBefore = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: false
+        }
+    ];
+    const action = {
+        type: 'TOGGLE_TODO',
+        id: 1
+    };
+    const stateAfter = [
+        {
+            id: 0,
+            text: 'Learn Redux',
+            completed: false
+        },
+        {
+            id: 1,
+            text: 'Go Shopping',
+            completed: true
+        }
+    ];
+
+    deepFreeze(stateBefore);
+    deepFreeze(action);
+
+    expect(
+        todos(stateBefore, action)
+    ).toEqual(stateAfter);
+}
+
 testAddTodo();
+testToggleTodo();
 console.log('All tests passed.');
 
 const TodoApp = () => (<div>Initial Refactor Commit</div>)
