0ef54a489d2c264e01bc676555bfb6499ceb8cd2
Dhaval <dhaval.makvana@techmatters.com>
2019-07-24 05:17:06 +0530

    reducer composition

--- src/index.js
+++ src/index.js
@@ -1,105 +1,106 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import expect from 'expect';
-import deepFreeze from 'deep-freeze';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import expect from "expect";
+import deepFreeze from "deep-freeze";
+
+const todo = (state, action) => {
+	switch (action.type) {
+		case "ADD_TODO":
+			return {
+				id: action.id,
+				text: action.text,
+				completed: false
+			};
+		case "TOGGLE_TODO":
+			if (state.id !== action.id) {
+				return state;
+			}
+			return {
+				...state,
+				completed: !state.completed
+			};
+		default:
+			return state;
+	}
+};
 
 const todos = (state = [], action) => {
-    switch (action.type) {
-        case 'ADD_TODO':
-            return [
-                ...state,
-                {
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                }
-            ];
-        case 'TOGGLE_TODO':
-            return state.map((todo) => {
-                if (todo.id !== action.id) {
-                    return todo;
-                }
-                return {
-                    ...todo,
-                    completed: !todo.completed
-                }
-            })
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case "ADD_TODO":
+			return [...state, todo(undefined, action)];
+		case "TOGGLE_TODO":
+			return state.map(t => todo(t, action));
+		default:
+			return state;
+	}
 };
 
 const testAddTodo = () => {
-    const stateBefore = [];
-    const action = {
-        type: 'ADD_TODO',
-        id: 0,
-        text: 'Learn Redux'
-    };
-    const stateAfter = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        }
-    ];
+	const stateBefore = [];
+	const action = {
+		type: "ADD_TODO",
+		id: 0,
+		text: "Learn Redux"
+	};
+	const stateAfter = [
+		{
+			id: 0,
+			text: "Learn Redux",
+			completed: false
+		}
+	];
 
-    deepFreeze(stateBefore);
-    deepFreeze(action);
+	deepFreeze(stateBefore);
+	deepFreeze(action);
 
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
+	expect(todos(stateBefore, action)).toEqual(stateAfter);
 };
 
 const testToggleTodo = () => {
-    const stateBefore = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        },
-        {
-            id: 1,
-            text: 'Go Shopping',
-            completed: false
-        }
-    ];
-    const action = {
-        type: 'TOGGLE_TODO',
-        id: 1
-    };
-    const stateAfter = [
-        {
-            id: 0,
-            text: 'Learn Redux',
-            completed: false
-        },
-        {
-            id: 1,
-            text: 'Go Shopping',
-            completed: true
-        }
-    ];
+	const stateBefore = [
+		{
+			id: 0,
+			text: "Learn Redux",
+			completed: false
+		},
+		{
+			id: 1,
+			text: "Go Shopping",
+			completed: false
+		}
+	];
+	const action = {
+		type: "TOGGLE_TODO",
+		id: 1
+	};
+	const stateAfter = [
+		{
+			id: 0,
+			text: "Learn Redux",
+			completed: false
+		},
+		{
+			id: 1,
+			text: "Go Shopping",
+			completed: true
+		}
+	];
 
-    deepFreeze(stateBefore);
-    deepFreeze(action);
+	deepFreeze(stateBefore);
+	deepFreeze(action);
 
-    expect(
-        todos(stateBefore, action)
-    ).toEqual(stateAfter);
-}
+	expect(todos(stateBefore, action)).toEqual(stateAfter);
+};
 
 testAddTodo();
 testToggleTodo();
-console.log('All tests passed.');
+console.log("All tests passed.");
 
-const TodoApp = () => (<div>Initial Refactor Commit</div>)
+const TodoApp = () => <div>Initial Refactor Commit</div>;
 
-
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
